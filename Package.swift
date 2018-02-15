bbbca3e0f11bee1593b817899809f89277fa54fc
trnserdar <serdar.turan@bodevoffice.com>
2018-02-15 11:22:53 +0300

    added Perfect-Mustache dependency in Package.swift file.

--- Package.swift
+++ Package.swift
@@ -7,6 +7,7 @@ let package = Package(
 	dependencies: [
         .Package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git", majorVersion: 2),
 		.Package(url: "https://github.com/PerfectlySoft/Perfect-MySQL.git", majorVersion: 2),
-        .Package(url: "https://github.com/SwiftORM/MySQL-StORM.git", majorVersion: 1)
+        .Package(url: "https://github.com/SwiftORM/MySQL-StORM.git", majorVersion: 1),
+        .Package(url: "https://github.com/PerfectlySoft/Perfect-Mustache.git", majorVersion: 2)
 	]
 )
